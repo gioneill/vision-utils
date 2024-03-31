cf177c841cdfa1557899821598fb46d5ac29c497
Gregory O'Neill <gioneill@gmail.com>
2024-03-31 13:48:52 -0700

    create test of FOV to figure out ideal setting for snap spectacles v3

--- picCombiner/picCombiner/main.swift
+++ picCombiner/picCombiner/main.swift
@@ -43,39 +43,47 @@ struct PicCombiner: ParsableCommand {
     }
     
     func combineImages(leftImg: CGImage, rightImg: CGImage, outputPath: String) {
-        let newImageURL = URL(fileURLWithPath: outputPath)
-        let destination = CGImageDestinationCreateWithURL(newImageURL as CFURL, UTType.heic.identifier as CFString, 2, nil)!
         
-        let imageWidth = CGFloat(leftImg.width)
-        let imageHeight = CGFloat(leftImg.height)
-        let fovHorizontalDegrees: CGFloat = 55
-        let fovHorizontalRadians = fovHorizontalDegrees * (.pi / 180)
-        let focalLengthPixels = 0.5 * imageWidth / tan(0.5 * fovHorizontalRadians)
-        let baseline = 65.0 // in millimeters
-
-        let cameraIntrinsics: [CGFloat] = [
-            focalLengthPixels, 0, imageWidth / 2,
-            0, focalLengthPixels, imageHeight / 2,
-            0, 0, 1
-        ]
-
-        let properties = [
-            kCGImagePropertyGroups: [
-                kCGImagePropertyGroupIndex: 0,
-                kCGImagePropertyGroupType: kCGImagePropertyGroupTypeStereoPair,
-                kCGImagePropertyGroupImageIndexLeft: 0,
-                kCGImagePropertyGroupImageIndexRight: 1,
-            ],
-            kCGImagePropertyHEIFDictionary: [
-                kIIOMetadata_CameraModelKey: [
-                    kIIOCameraModel_Intrinsics: cameraIntrinsics as CFArray
+        var fov = 30
+        
+        for _ in 0..<30 {
+            let outputPath = outputPath.appending("\(-fov).heic")
+            let newImageURL = URL(fileURLWithPath: outputPath)
+            let destination = CGImageDestinationCreateWithURL(newImageURL as CFURL, UTType.heic.identifier as CFString, 2, nil)!
+            
+            let imageWidth = CGFloat(leftImg.width)
+            let imageHeight = CGFloat(leftImg.height)
+            let fovHorizontalDegrees = CGFloat(fov)
+            let fovHorizontalRadians = fovHorizontalDegrees * (.pi / 180)
+            let focalLengthPixels = 0.5 * imageWidth / tan(0.5 * fovHorizontalRadians)
+            let baseline = 65.0 // in millimeters
+            
+            let cameraIntrinsics: [CGFloat] = [
+                focalLengthPixels, 0, imageWidth / 2,
+                0, focalLengthPixels, imageHeight / 2,
+                0, 0, 1
+            ]
+            
+            let properties = [
+                kCGImagePropertyGroups: [
+                    kCGImagePropertyGroupIndex: 0,
+                    kCGImagePropertyGroupType: kCGImagePropertyGroupTypeStereoPair,
+                    kCGImagePropertyGroupImageIndexLeft: 0,
+                    kCGImagePropertyGroupImageIndexRight: 1,
+                ],
+                kCGImagePropertyHEIFDictionary: [
+                    kIIOMetadata_CameraModelKey: [
+                        kIIOCameraModel_Intrinsics: cameraIntrinsics as CFArray
+                    ]
                 ]
             ]
-        ]
-
-        CGImageDestinationAddImage(destination, leftImg, properties as CFDictionary)
-        CGImageDestinationAddImage(destination, rightImg, properties as CFDictionary)
-        CGImageDestinationFinalize(destination)
+            
+            CGImageDestinationAddImage(destination, leftImg, properties as CFDictionary)
+            CGImageDestinationAddImage(destination, rightImg, properties as CFDictionary)
+            CGImageDestinationFinalize(destination)
+            
+            fov += 5
+        }
     }
 }
 
